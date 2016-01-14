fe808906af71c45045f86864822c5f7f92b2fba1
Avneet Kour <avneet.aryans@gmail.com>
2016-01-14 22:25:56 +0530

    Made sidebars - footer and project portion dynamic

--- functions.php
+++ functions.php
@@ -42,9 +42,34 @@ anytime query_posts is used, it overrides $wp_query global hence it is not advic
     }
 
 	add_action( 'after_setup_theme', 'register_theme_menu' );
+
 	function register_theme_menu() {
+		
 	  add_theme_support( 'post-thumbnails' ); 	
+		
 	  register_nav_menu( 'main-menu', __( 'Awesome Primary Menu', 'awesome' ) );
+		
+ 	  register_sidebar( array(
+        'name' => __( 'Projects Sidebar', 'awesome' ),
+        'id' => 'sidebar-projects',
+        'description' => __( 'This sidebar is for projects on the home page' ),
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+			) 
+  		);
+		
+	  register_sidebar( array(
+	  'name' => __( 'Footer Sidebar', 'awesome' ),
+	  'id' => 'sidebar-footer',
+	  'description' => __( 'This sidebar is for footer on the home page' ),
+	  'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	  'after_widget'  => '</li>',
+	  'before_title'  => '<h2 class="widgettitle">',
+	  'after_title'   => '</h2>',
+			) 
+		);
 	}
 
     function say_hello_world($default) {
@@ -73,3 +98,31 @@ anytime query_posts is used, it overrides $wp_query global hence it is not advic
     }
     //add_filter('pre_get_posts','alter_home_page_posts');
     
+	add_action( 'init', 'create_post_type');
+
+	function create_post_type() {
+		register_post_type( 'players',
+			array(
+			  'labels' => array(
+				'name' => __( 'Players' ),
+				'singular_name' => __( 'Player' )
+			  ),
+			  'public' => true,
+			  'has_archive' => true,
+			)
+		);
+		
+		register_taxonomy( 
+			'team',
+		  'players',
+		  array(
+			'label' => __( 'Teams', 'taxonomy general name' ),
+			'labels' => array(
+				'name' => _x( 'Teams', 'taxonomy general name'  ),
+				'singular_name' => _x( 'Team' , 'taxonomy singular name'  )
+			  ),  
+			'rewrite' => array( 'slug' => 'Team' ),
+			'hierarchical' => true
+			)
+		 );
+   }
---
+++ sidebar.php
@@ -0,0 +1,6 @@
+<div class="container pt">
+	<?php if ( is_active_sidebar( 'sidebar-projects' ) ) : ?>
+		<?php dynamic_sidebar( 'sidebar-projects' ); ?>
+	<?php endif; ?>
+</div>
+
