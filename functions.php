ee3e53a42f22e2958a81daaac0b4e5f72f012be5
Avneet Kour <avneet.aryans@gmail.com>
2016-01-16 18:17:57 +0530

    Added custom post-type, taxonomy and meta box

--- functions.php
+++ functions.php
@@ -101,7 +101,8 @@ anytime query_posts is used, it overrides $wp_query global hence it is not advic
 	add_action( 'init', 'create_post_type');
 
 	function create_post_type() {
-		register_post_type( 'players',
+		register_post_type( 
+			'players',
 			array(
 			  'labels' => array(
 				'name' => __( 'Players' ),
@@ -126,3 +127,110 @@ anytime query_posts is used, it overrides $wp_query global hence it is not advic
 			)
 		 );
    }
+
+	add_action('add_meta_boxes', 'awesome_add_meta_boxes');
+	
+	function awesome_add_meta_boxes() {
+		add_meta_box(
+			'player_info_id', // ID 
+			__('Add Player Info', 'awesome'), // Title
+			'player_info', // callback function
+			'players', // post - type (default/custom)
+			'normal', // context
+			'high' // priority
+		);
+	}
+
+	function player_info($post) {
+		
+		global $post;
+		
+		wp_nonce_field( basename( __FILE__ ), 'player_info_nonce' ); ?>
+
+		<p>
+			<label for="player_age">
+				<?php _e('Player Age','awesome'); ?>
+			</label>
+			<input type="text" name="player_age" id="player_age" value="<?php echo get_post_meta( $post->ID, 'player_age', true) ; ?>" /><br>
+			
+			<label for="total_matches">
+				<?php _e('Total Matches','awesome'); ?>
+			</label>
+			<input type="text" name="total_matches" id="total_matches" value="<?php echo get_post_meta( $post->ID, 'total_matches', true); ?>" /><br>
+			
+			<label for="player_name">
+				<?php _e('Total Runs','awesome'); ?>
+			</label>
+			<input type="text" name="total_runs" id="total_runs" value="<?php echo get_post_meta( $post->ID, 'total_runs', true); ?>" /><br>
+			
+			<label for="best_score">
+				<?php _e('Best Score','awesome'); ?>
+			</label>
+			<input type="text" name="best_score" id="best_score" value="<?php echo get_post_meta( $post->ID, 'best_score', true); ?>" /><br>
+			
+			<label for="player_type">
+				<?php _e('Player Type','awesome'); ?>
+			</label>
+			<input type="radio" name="player_type" value="batsman" <?php checked( get_post_meta( $post->ID, 'player_type', true) == "batsman"); ?> > <?php _e('Batsman','awesome'); ?><br>
+  			<input type="radio" name="player_type" value="bowler" <?php checked( get_post_meta( $post->ID, 'player_type', true) == "bowler"); ?> > <?php _e('Bowler','awesome'); ?><br>
+  			<input type="radio" name="player_type" value="all_rounder" <?php checked( get_post_meta( $post->ID, 'player_type', true) == "all_rounder"); ?> > <?php _e('All-Rounder','awesome'); ?><br>
+		</p> <?php 
+	}
+
+	function save_player_info($post_id) {
+		
+		/*
+		 * We need to verify this came from our screen and with proper authorization,
+		 * because the save_post action can be triggered at other times.         
+		 */
+
+		// Check if our nonce is set.
+		if ( ! isset( $_POST['player_info_nonce'] ) ) {
+			return;
+		}
+
+		// Verify that the nonce is valid.
+		if ( ! wp_verify_nonce( $_POST['player_info_nonce'], basename( __FILE__ ) ) ) {
+			return;
+		}
+
+		// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		// Check the user's permissions.
+		if ( isset( $_POST['post_type'] ) && 'players' == $_POST['post_type'] ) {
+
+			if ( ! current_user_can( 'edit_post', $post_id ) ) {
+				return;
+			}
+
+		}
+
+		/* OK, it's safe for us to save the data now. */
+
+		// Make sure that it is set.
+		if ( ! isset( $_POST ) ) {
+			return;
+		}
+		
+		$player_info['player_age'] = $_POST['player_age'];
+		$player_info['total_matches'] = $_POST['total_matches'];
+		$player_info['total_runs'] = $_POST['total_runs'];
+	    $player_info['best_score'] = $_POST['best_score'];
+		$player_info['player_type'] = $_POST['player_type'];
+		
+		foreach($player_info as $key => $value){	
+			
+			// Sanitize user input.
+			$value = sanitize_text_field( $value );
+			   
+			// Update the meta field in the database.
+			update_post_meta( $post_id, $key, $value);
+		}
+	
+	}
+	add_action('save_post','save_player_info');
+
+?>
